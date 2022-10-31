271646f57709acae27fdff9198de798fe0f58d00
advent22 <adventsukses22@gmail.com>
2022-10-31 14:43:31 +0700

    first commit

---
+++ Main.java
@@ -0,0 +1,28 @@
+package TugasPraktek;
+import java.util.Scanner;
+
+public class Main implements MencariNilaiGanjilGenap {
+
+// deklarasi constructor
+public Main(){
+// TODO Auto-generated constructor stub
+}
+
+// menerapkan function mencarinilaiganjilgenap
+public void cariNilaiGanjilGenap(int inputan_angka){
+
+}
+
+// membuat function main
+public static void main(String[] args){
+
+
+// deklarasi variable
+int input_angka;
+
+// inisialisasi class objek
+Scanner scanner = new Scanner(System.in);
+
+}
+
+}
---
+++ MencariNilaiGanjilGenap.java
@@ -0,0 +1,6 @@
+package TugasPraktek;
+
+
+public interface MencariNilaiGanjilGenap {
+public void cariNilaiGanjilGenap(int inputan_angka);
+}
